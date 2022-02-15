fef0fd2e1c771b2b471786ad8b99c8526ebb4de1
Sijan04 <sijansidda4@gmail.com>
2022-02-15 13:58:07 -0800

    MID_LAB_TASK_06_PHP_SESSION

---
+++ MID_LAB_TASK_06_PHP_SESSION/Forget password.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+	<title>Forget password</title>
+</head>
+<body>
+
+	<form method="POST" action="loginCheck.php">
+		<fieldset>
+			<legend>Login</legend>
+			<table>
+				<tr>
+					<td>Username</td>
+					<td><input type="text" name="Enter email" value=""></td>
+				</tr>
+			
+				<tr>
+					<td></td>
+					<td><input type="submit" name="submit" value="Submit"></td>
+				</tr>
+			</table>
+		</fieldset>
+	</form>
+</body>
+</html> 
---
+++ MID_LAB_TASK_06_PHP_SESSION/public home.php
@@ -0,0 +1,15 @@
+
+<html>
+<head>
+       <title>Public Home</title>
+</head>
+<body>
+       <h1>Welcome to XCompany!</h1> <a href="login.php"> Login </a><a href="Home.php"> Home </a><a href="Registration.php"> Registration </a>
+</body>
+</html>
+
+<?php
+       }else{
+              header('location: login.php');
+       }
+?>
